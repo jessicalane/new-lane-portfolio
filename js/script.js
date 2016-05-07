8d1797474aa13ac8d66e1cd6fc5df33df5354079
Jessica Lane <jessica.lynn.brasher@gmail.com>
2016-05-06 22:38:30 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var app = angular.module('myModule', ['ngRoute']);
+
+app.controller('myController', function() {
+
+});
+
+app.config(function($routeProvider) {
+	$routeProvider('/', {
+		
+	});
+	
+});
