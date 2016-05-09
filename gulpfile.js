22248557eaf8f4656f15f8c3d6a1b73222ff9206
Jessica Lane <jessica.lynn.brasher@gmail.com>
2016-05-08 20:02:12 -0400

    added & linked bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 var gulp = require('gulp');
 	jshint = require('gulp-jshint');
+	sass = require('gulp-sass');
 
 gulp.task('lint', function() {
-	return gulp.src('./js/*.js')
-		// .pipe(jshint()) // Is this line necessary?
+	return gulp.src('/js/*.js')
+		.pipe(jshint()) // Is this line necessary?
 		.pipe(jshint.reporter('default'));
 });
 
-gulp.task('default', function() {
+gulp.task('sass', function() {
+	gulp.src('./styles/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./css'))
+});
+
+gulp.task('default', ['lint'], function() {
 
 });
