a8016af0ffd2b5dd51b69aaecf0c8543c928c5e7
Jessica Lane <jessica.lynn.brasher@gmail.com>
2016-05-11 21:58:48 -0400

    Changed styles and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
 var gulp = require('gulp');
 	jshint = require('gulp-jshint');
+	stylus = require('gulp-stylus')
 
 gulp.task('lint', function() {
 	return gulp.src('/js/*.js')
 		.pipe(jshint()) // Is this line necessary?
 		.pipe(jshint.reporter('default'));
+});
+
+gulp.task('styles', function () {
+	gulp.src('public/styles/styles.styl')
+		.pipe(stylus())
+		.pipe(gulp.dest('public/styles/css'));
+});
+
+gulp.task('watch:styles', function () {
+	gulp.watch('public/styles/*.styl', ['styles']);
 });
 
 gulp.task('default', ['lint'], function() {
